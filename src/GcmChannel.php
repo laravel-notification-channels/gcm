488b3700d681b0792c9c33596882615030f44cac
Drilon <dsaraqini@adneom.com>
2016-09-02 16:05:43 +0200

    Mirate to FCM & added the missing parameters

--- src/GcmChannel.php
+++ src/GcmChannel.php
@@ -73,10 +73,11 @@ class GcmChannel
         $packet = new Packet();
 
         $packet->setRegistrationIds($tokens);
+        $packet->setPriority($message->priority);
         $packet->setCollapseKey(str_slug($message->title));
-        $packet->setData([
+        $packet->setNotification([
                 'title' => $message->title,
-                'message' => $message->message,
+                'body' => $message->message,
             ] + $message->data);
 
         return $packet;
