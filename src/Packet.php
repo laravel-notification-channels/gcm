99fd4ae6f521b59f9ecb28ec5271c73d8ca8a2ba
Azri Ismail <azri.ismail@nazrol.tech>
2017-05-06 22:34:59 +0800

    bump zendframework/zendservice-google-gcm version which update to use fcm

--- src/Packet.php
+++ src/Packet.php
@@ -15,10 +15,10 @@ class Packet extends Message
     /**
      * Set the notification.
      *
-     * @param array $ids
+     * @param array $notification
      * @return Message
      */
-    public function setNotification($notification)
+    public function setNotification(array $notification)
     {
         $this->notification = $notification;
 
