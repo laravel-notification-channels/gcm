59d817d9ce9a403dc8a1bd9184ffee662c60412f
Joe Lennon <joe.lennon@workvivo.com>
2019-03-02 13:05:00 +0000

    Updated TestCase with return type to work with new version of PHPUnit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function tearDown()
+    public function tearDown(): void
     {
         parent::tearDown();
 
